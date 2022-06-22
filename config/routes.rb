aa8b4159081a981ae3cc33c88b194f539e4fd795
alina-paik <alinapaik143@gmail.com>
2022-06-22 22:54:11 +0300

    update entity

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+  devise_for :users
+  get 'persons/profile', as: 'user_root'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
